27e07d8073a4ec4506f40471b3ef8fdea447ca5e
Wael Karman <wael.karman@gmail.com>
2024-04-16 14:05:31 +0200

    fix

--- gpio-read-sysfs-service.cpp
+++ gpio-read-sysfs-service.cpp
@@ -15,51 +15,39 @@
 #include "zhelpers.hpp"
 
 
-#define GPIO_PIN "18" //GPIO pin (BCM number) used as an input
+#define GPIO_PIN "18"
 
 int main() {
-
-	int status; //Create a status variable
-	GPIO gpio_test(GPIO_PIN); //Create a GPIO object
-
+	int status; 
+	GPIO gpio_test(GPIO_PIN); 
 
 	zmq::context_t context(1);
     zmq::socket_t publisher(context, ZMQ_PUB);
     //  Initialize random number generator
     srandom ((unsigned) time (NULL));
 	publisher.bind("tcp://*:5554");
-    //  Ensure subscriber connection has time to complete
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+	status = gpio_test.setupPin(1);
+	if (status != 0) return 1;
 
+	status = gpio_test.setDirection(0);
+	if (status != 0) return 1; 
 	while(1){
-		status = gpio_test.setupPin(1); //Create pin
-		if (status != 0) return 1; //Return error code
-
-		status = gpio_test.setDirection(0); //Set pin direction
-		if (status != 0) return 1; //Return error code
-
 		std::string res;
-		status = gpio_test.readValue(&res); //Set pin value
-		if (status != 0) return 1; //Return error code
+		status = gpio_test.readValue(&res); 
+		if (status != 0) return 1; 
 
-		if (res == "1") {
-			//std::cout << "GPIO pin " << GPIO_PIN << " is HIGH\n";
-			std::this_thread::sleep_for(std::chrono::seconds(1));
-			s_sendmore (publisher, "BUTTON");
+		s_sendmore (publisher, "BUTTON");
+		if (res == "1") {	
 			s_send (publisher, "Button RELEASED");
-
 		}
 		else {
-			//std::cout << "GPIO pin " << GPIO_PIN << " is LOW\n";
-			std::this_thread::sleep_for(std::chrono::seconds(1));
-			s_sendmore (publisher, "BUTTON");
 			s_send (publisher, "Button PRESSED");
 		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 
-	status = gpio_test.setupPin(0); //Disable pin
-	if (status != 0) return 1; //Return error code
+	status = gpio_test.setupPin(0);
+	if (status != 0) return 1;
 
-
-	return 0; //Return default code
+	return 0; 
 }
